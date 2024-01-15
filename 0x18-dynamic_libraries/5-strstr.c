2113c6e50eb26584d4e772d0ff73c10de1cfbb49
IO <issam_ouzali@outlook.fr>
2024-01-15 04:46:23 -0800

    all c files

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - prints the consecutive caracters of s1 that are in s2.
+ * @haystack: source string
+ * @needle: searching string
+ *
+ * Return: new string.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *n;
+
+	if (haystack == '\0' || needle == '\0')
+		return (0);
+	for (; *haystack; haystack++)
+	{
+
+	for (h = haystack, n = needle; *h && *n && (*h == *n); h++, n++)
+		;
+	if (*n == '\0')
+		return (haystack);
+	}
+	return (0);
+}
